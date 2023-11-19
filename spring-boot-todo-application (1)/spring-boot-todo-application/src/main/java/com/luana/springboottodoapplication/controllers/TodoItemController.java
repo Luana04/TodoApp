c8b943ed72e3b1557afeac3cbb3ca92c505c6c5b
Luana04 <68239153+Luana04@users.noreply.github.com>
2023-11-19 23:05:46 +0100

    Functionality for edit, delete and complete

--- spring-boot-todo-application (1)/spring-boot-todo-application/src/main/java/com/luana/springboottodoapplication/controllers/TodoItemController.java
+++ spring-boot-todo-application (1)/spring-boot-todo-application/src/main/java/com/luana/springboottodoapplication/controllers/TodoItemController.java
@@ -1,11 +1,18 @@
 package com.luana.springboottodoapplication.controllers;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.luana.springboottodoapplication.models.TodoItem;
 import com.luana.springboottodoapplication.repositories.TodoItemRepository;
 
 
@@ -22,6 +29,15 @@ public class TodoItemController {
         return modelAndView;
     }
 
-    
+    @PostMapping("/todo/{id}")
+    public String updateTodoItem(@PathVariable("id") long id, @Valid TodoItem todoItem, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
 
 }
